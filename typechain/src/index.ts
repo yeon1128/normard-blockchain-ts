a0f3b638c0949c9156433eaae09dc237d72260a5
jeongyeon.seo <s9530348@naver.com>
2023-01-30 16:10:51 +0900

    build: 타입 스크립트 프로젝트 생성

---
+++ typechain/src/index.ts
@@ -0,0 +1,11 @@
+class Block {
+  constructor(private data: string) {}
+  static hello() {
+    return "hi";
+  }
+}
+
+// window
+// document.querySelector();
+// localStorage.getItem();
+// Math.ceil();
