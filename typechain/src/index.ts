e68898abdc1fdf58d3eea70767a6a0fa38a76c44
jeongyeon.seo <s9530348@naver.com>
2023-01-30 17:18:12 +0900

    add: 블록체인 프로젝트 시작

--- typechain/src/index.ts
+++ typechain/src/index.ts
@@ -1,18 +1,22 @@
-class Block {
-  constructor(private data: string) {}
-  static hello() {
-    return "hi";
+// import * as crypto from "crypto";
+import crypto from "crypto";
+
+interface BlockShape {
+  hash: string;
+  prevHash: string;
+  height: number;
+  data: string;
+}
+class Block implements BlockShape {
+  public hash: string;
+  constructor(
+    public prevHash: string,
+    public height: number,
+    public data: string
+  ) {
+    this.hash = Block.calculateHash(prevHash, height, data);
+  }
+  static calculateHash(prevHash: string, height: number, data: string) {
+    const toHash = `${prevHash}${height}${data}`;
   }
 }
-
-// window
-// document.querySelector();
-// localStorage.getItem();
-// Math.ceil();
-
-// import { init, exit } from "myPackage";
-// init({ url: "true" });
-// exit(1);
-
-import { init, exit } from "./myPackage";
-// init();
